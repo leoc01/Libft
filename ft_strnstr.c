c4074d2a2283e9e3fa333aea06edfef90becca32
Leonardo Buscarons Campetti <lbuscaro@c2r3s9.42porto.com>
2025-04-18 20:14:24 +0100

    feat: add `ft_strnstr` function and test

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -14,8 +14,28 @@
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	(void)big;
-	(void)little;
-	(void)len;
+	int	i;
+	int	j;
+
+	if (little[0] == '\0')
+	{
+		return ((char *)big);
+	}
+	i = 0;
+	j = 0;
+	while (big[i] != '\0' && len > 0)
+	{
+		while (big[i + j] == little[j])
+		{
+			j++;
+			if (little[j] == '\0')
+			{
+				return (&((char *)big)[i]);
+			}
+		}
+		j = 0;
+		i++;
+		len--;
+	}
 	return (NULL);
 }
--- tests/ft_strnstr_test.c
+++ tests/ft_strnstr_test.c
@@ -12,7 +12,35 @@
 
 #include "libftest.h"
 
+static int	runtest(const char *big, const char *little, size_t n);
+
 int	ft_strnstr_test(void)
 {
-	return (FAIL);
+	if (!runtest("a nice test", "test", 30))
+		return (FAIL);
+	if (!runtest("a nice test", "test", 0))
+		return (FAIL);
+	if (!runtest("a nice test", "oi", 30))
+		return (FAIL);
+	if (!runtest("a nice test", "A n", 30))
+		return (FAIL);
+	if (!runtest("a nice test", "", 30))
+		return (FAIL);
+	if (!runtest("", "test", 30))
+		return (FAIL);
+	return (OK);
+}
+
+static int	runtest(const char *big, const char *little, size_t n)
+{
+	char	*res_o;
+	char	*res_m;
+
+	res_o = strnstr(big, little, n);
+	res_m = ft_strnstr(big, little, n);
+	if (res_o != res_m)
+	{
+		return (FAIL);
+	}
+	return (OK);
 }
